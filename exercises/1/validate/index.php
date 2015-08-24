4750e32e84d190ab146c5dbd63bc6de6d6ed8920
Anton Tibblin <tibbelit@gmail.com>
2015-08-24 15:14:06 +0200

    Added exercise - Validate

--- exercises/1/validate/index.php
+++ exercises/1/validate/index.php
@@ -1,5 +1,15 @@
-<h2>Att validera</h2>
-<p>Det är viktigt att de webbsidor som man skriver validerar - alltså följer de standarder som finns. Vi vill ju att alla följer samma regler.</p>
-<p>I denna övning finns en ZIP-fil med 5st HTML-dokument som inte validerar, er uppgift är att rätta till de så att de validerar - en bra kunskap att ha.</p>
+<h2>Övning - Att validera HTML-kod</h2>
+<p>Det är viktigt att de webbsidor som man skriver validerar - alltså följer de standarder som finns. Vi vill ju att alla följer samma regler, bland annat för att:</p>
+<ul>
+    <li>Det skapar kvalité på koden</li>
+    <li>Webbsidan kommer visas på samma sätt i olika webbläsare</li>
+    <li>Om man samarbetar kring koden är det viktigt att alla har samma inställning till att skriva bra kod</li>
+</ul>
+<h3>1. Övningen</h3>
+<p>I denna övning finns en ZIP-fil med 3st HTML-dokument som inte validerar, er uppgift är att rätta till de så att de validerar - en bra kunskap att ha med er till kommande inlämningsuppgifter.</p>
 <p>Ni validerar koden här: <a href="http://validator.w3.org/">http://validator.w3.org/</a></p>
-<p><a href="/exercises/1/validate/ex.zip">Ni laddar ner övningsfilerna här</</p>
+<p><a href="/exercises/1/validate/Validate.zip">Ni laddar ner övningsfilerna här (ZIP-fil)</a></p>
+
+
+<h3>2. Facit</h3>
+<p>Facit för hur HTML-dokumentet ska se ut hittar ni <a href="/exercises/1/validate/facit.html">här</a>.</p>
