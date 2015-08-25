9b7ca01920e0001cd6adbf8405a0334b153ce466
Anton Tibblin <tibbelit@gmail.com>
2015-08-25 14:51:11 +0200

    Added Todo-list + fixed broken link

--- modules/1/index.php
+++ modules/1/index.php
@@ -76,7 +76,7 @@
             <li><a href="/exercises.php?module=1&title=Text">Text (paragrafer, rubriker, listor)</a></li>
             <li><a href="/exercises.php?module=1&title=ImagesLinks">Bilder & länkar</a></li>
             <li><a href="/exercises.php?module=1&title=Indentering">Indentering</a></li>
-            <li><a href="/exercises.php?module=1&title=validate">Validera HTML</a></li>
+            <li><a href="/exercises.php?module=1&title=Validate">Validera HTML</a></li>
         </ol>
    </div>
    <div class="col-lg-4">
