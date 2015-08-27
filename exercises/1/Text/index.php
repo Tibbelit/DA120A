60cd0ef004f30fb6e5c03f44b8fcb8053ff82f64
Jalamaal <bravoantonius@gmail.com>
2015-08-27 18:41:32 +0200

    Added link in excercise 1

--- exercises/1/Text/index.php
+++ exercises/1/Text/index.php
@@ -61,4 +61,4 @@
 
 <p>Har du klarat detta är du på god väg att ha skapat din första hemsida à 1998 style! Oroa dig inte, det kommer bli bättre med tiden. Resultatet än så länge bör se ut ungefär såhär: 
     <br>
-    <a href="/exercises/1/Text/ex1.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/text/ex1.png" style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;">.</p><!--Vill gärna ha in facit html men vet ej hur--!>
+    <a href="/exercises/1/Text/ex1.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/text/ex1.png" style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;">.</p>
