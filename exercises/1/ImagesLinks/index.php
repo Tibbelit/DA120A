380d7b87c04a1315821d183e3b01b92e3d4e297d
Jalamaal <bravoantonius@gmail.com>
2015-08-28 11:53:54 +0200

    Added content and changes to Images-tutorial

--- exercises/1/ImagesLinks/index.php
+++ exercises/1/ImagesLinks/index.php
@@ -1,12 +1,12 @@
 <h2>Övning 3 - Bilder & länkar</h2>
     <h3>1. Fräscha upp hemsidan</h3>
-        <p>Vegano har övervägt och man önskar ha smakfull bild på restaurangen samt ett par länkar till de vanligaste sociala medierna som man använder:                facebook, twitter och instagram.</p>
+        <p>Vegano har övervägt och man önskar ha smakfull bild på restaurangen samt ett par länkar till de vanligaste sociala medierna som man använder:                facebook, twitter och instagram. Man skulle även vilja ha en länk som går till en del av hemsidan som visar deras öppettider.</p>
 
         <p>Vi ska i denna övning utgå från föregående kod som vi har lyckats få ihop hittills. Resultatet efter våra tillägg bör rimligen se ut på detta                vis:</p>
             <p><a href="/exercises/1/ImagesLinks/ex2.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/ImagesLinks/ex2.png"                    style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;">.</a></p>
-    <h3>2. Bild</h2>
+    <h3>2. Huvudbild - absolut sökväg</h2>
         <p>Det första vi kan börja med är att leta upp en trevlig bild på en restaurang. Har du ingen inspiration kan du ta denna 
-            <a href="http://www.svenskamassan.se/Global/svenskamassan/bilder/restaurang.jpg">bild</a>. Du kan antingenanvända länken direkt till bildtaggen             eller ladda ner den och länka till bilden i din mapp. Vegano önskar ha bilden precis under sina huvudrubriker, ungefär såhär.</p>
+            <a href="http://www.svenskamassan.se/Global/svenskamassan/bilder/restaurang.jpg">bild</a>. Använd hela länkens adress och stoppa i den i din tagg som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#212-Absoluta-sokvagar">absolut sökväg</a></i>. Vegano önskar ha bilden precis under sina huvudrubriker, ungefär såhär.</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
 &lt;!doctype html&gt;
 &lt;html&gt;
@@ -21,45 +21,31 @@
 &lt;/html&gt;
 </code></pre> <!--Kan man gömma detta och låta studenten trycka på en knapp för att se lösningen?-->
 
-<h3>3. Rubriker</h3>
-<p>Nu när vi fått ordning på informationen om dokumentet ifråga ska vi börja med det inledande innehållet. Vi går nedåt ett snäpp från huvudet och ner på resten av <i>kroppen</i> (eng. body). Därför behöver vi definiera var detta innehåll ska vara.</p>
+<h3>3. Ikoner - relativ sökväg</h3>
+<p>En bild säger mer än tusen rader kod brukar det sägas, vilket lyft! Nästa utmaning vi ska ta oss an är att lägga upp ikonerna för facebook, twitter och instagram på slutet av sidan och sedan gör dem klickbara. Vi börjar med ikonerna. Till skillnad från föregående bild ska vi nu försöka länka dem direkt från en mapp på datorn, det vill säga som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#211-Relativa-sokvagar">relativ sökväg</a></i>. Varje ikon kommer behöva en egen <code>&lt;img&gt;</code> tagg! Ladda ner följande zipfil och extrahera den i samma mapp som ditt HTML-dokument. Sökvägen bör se ut ungefär såhär:</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
-&lt;head&gt;
-    &lt;title&gt;&lt;/title&gt;
-&lt;/head&gt;
-&lt;body&gt;
-
-&lt;/body&gt;
+&lt;img src="images/namnet-på-din-bild"&gt;
 </code></pre>
-<p>Försök nu att placera in en passande rubrik mellan 'kropp'-taggarna samt en mindre rubrik nedanför den stora med en slogan. Storlek på rubriken bestäms från h1 (störst) till h6 (minst).
-<pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
-&lt;h1&gt;&lt;h2&gt;&lt;h3&gt;&lt;h4&gt;&lt;h5&gt;&lt;h6&gt;
-</code></pre>
+<p>Placera nu in ikonerna på rätt plats i dokumentet!
     
 <!-- Sätt in exempel -->
 
 
-<h3>3. Presentation och paragrafer</h3>
-<p>Förhoppningsvis sitter rubriken på plats! Nu är det dags att skriva en presentation och placera den i rätt ordning. Restaurangen har önskat en egen presentation, men du får gärna strunta i vad de tycker och skriva en bättre om du önskar. Använd paragraf-taggar för presentationen!</p>
-
+<h3>3. Klickbara ikoner</h3>
+<p>I <a href="http://localhost:1234/lectures.php?type=Modul1&nr=Links">länkföreläsningen</a> bekantade vi oss med <code>&lt;a&gt;</code> (står för eng. <i>anchor</i>) där vi kan ange relativa eller absoluta sökvägar till en annan plats. Det roliga är att bilder/ikoner också kan vara länkar! För att göra en klickbara ikoner behöver vi definiera dels vad det är som klickas på samt var vi hamnar när vi klickar på det. Du behöver därför "nästla" (eng. <i>nesting</i>) ikonerna innanför din <code>&lt;a&gt;</code> tagg: </p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
-&lt;p&gt;&lt;/p&gt;
+&lt;a href="länk-till-annan-sida"&gt;&lt;img src="images/namnet-på-din-bild"&gt;&lt;/a&gt;
 </code></pre>
-<br>
-<p><i>"Vegano erbjuder helekologisk och vegetarisk lunchbuffé för endast 69 kronor. Under kvällstid har du som gäst möjlighet att boka ett Vin & Vego-bord med säsongsbetonad meny med passande viner till. Vi är belägna på Vildmåsvägen 111 i Lund, välkomna! Vår lunchmeny finner ni nedan."</i></p>
+<p>Använd gärna adresserna till rätt hemsida till varje ikon. Pröva sedan att byta ut adresserna och se vad som händer!</p>
 
-<h3>4. Menylista </h3>
-<p>Slutligen vill restaurangen att en del av deras lunchmeny ska finnas tillgänglig. Uppdelningen bör vara i rubriker med kalla och varma rätter med varsin punktad lista.</p>
+<h3>4. Öppettider </h3>
+<p>Eftersom du redan vid det här laget lyckats få klickbara ikoner så kommer denna delövning vara som en dagisövning. Det första vi behöver göra är att skapa ett nytt HTML-dokument i samma mapp som ditt nuvarande dokument. Du kan döpa den till <code>oppettider.html</code> exempelvis. Det viktiga är att du använder rätt namn senare till din relativa sökväg. Skapa sedan en enkel rubrik med "Våra öppettider" och denna gång får du välja själv när Vegano ska ha öppet.</p>
 
-<p>De kalla rätterna ska vara pasta romano, broccolipaj och vegetarisk pastrami. De varma rätterna ska vara svamprisotto, ärtlasagne och pasta arrabiata.   </p>
+<p>I ditt huvuddokument ska du nu lägga till en liknande rubrik med "Våra öppettider" precis under huvudbilden. Den ska agera ankare till vårt nyskapade dokument! Nästla nu en <code>&lt;a&gt;</code> tagg runt din rubrik.</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
-&lt;ul&gt;
-   &lt;li&gt;&lt;/li&gt;
-   &lt;li&gt;&lt;/li&gt;
-   &lt;li&gt;&lt;/li&gt;
-&lt;/ul&gt;
+&lt;a href="ditt-nya-html-dokument.html"&gt;
+   &lt;h2&gt;Namnet-på-din-länk&lt;/h2&gt;
+&lt;/a&gt;
 </code></pre>
 
-<p>Har du klarat detta är du på god väg att ha skapat din första hemsida à 1998 style! Oroa dig inte, det kommer bli bättre med tiden. Resultatet än så länge bör se ut ungefär såhär: 
-    <br>
-    <a href="/exercises/1/Text/ex1.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/text/ex1.png" style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;"></a>.</p><!--Vill gärna ha in facit html men vet ej hur--!>
+<p>Klarat det? Bra jobbat! Nu har vi lagt till lite trevligare estetik på sidan, mera interaktion och kommit ett steg närmare rätt århundrade. </p>
