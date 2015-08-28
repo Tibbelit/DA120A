9de330a9509c892439e0bcfb91621f8f514d4391
Jalamaal <bravoantonius@gmail.com>
2015-08-28 19:28:55 +0200

    Added changes to Images-Links-tutorial

--- exercises/1/ImagesLinks/index.php
+++ exercises/1/ImagesLinks/index.php
@@ -1,6 +1,6 @@
 <h2>Övning 3 - Bilder & länkar</h2>
     <h3>1. Fräscha upp hemsidan</h3>
-        <p>Vegano har övervägt och man önskar ha smakfull bild på restaurangen samt ett par länkar till de vanligaste sociala medierna som man använder:                facebook, twitter och instagram. Man skulle även vilja ha en länk som går till en del av hemsidan som visar deras öppettider.</p>
+        <p>Vegano har övervägt hur hemsidan kan bli mer tilltalande, och man därför önskar ha smakfull bild på restaurangen samt ett par länkar till de             vanligaste sociala medierna som man använder: facebook, twitter och instagram. Man skulle även vilja ha en länk som går till en del av hemsidan som         visar deras öppettider.</p>
 
         <p>Vi ska i denna övning utgå från föregående kod som vi har lyckats få ihop hittills. Resultatet efter våra tillägg bör rimligen se ut på detta                vis:</p>
             <p><a href="/exercises/1/ImagesLinks/ex2.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/ImagesLinks/ex2.png"                    style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;">.</a></p>
@@ -22,7 +22,7 @@
 </code></pre> <!--Kan man gömma detta och låta studenten trycka på en knapp för att se lösningen?-->
 
 <h3>3. Ikoner - relativ sökväg</h3>
-<p>En bild säger mer än tusen rader kod brukar det sägas, vilket lyft! Nästa utmaning vi ska ta oss an är att lägga upp ikonerna för facebook, twitter och instagram på slutet av sidan och sedan gör dem klickbara. Vi börjar med ikonerna. Till skillnad från föregående bild ska vi nu försöka länka dem direkt från en mapp på datorn, det vill säga som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#211-Relativa-sokvagar">relativ sökväg</a></i>. Varje ikon kommer behöva en egen <code>&lt;img&gt;</code> tagg! Ladda ner följande zipfil och extrahera den i samma mapp som ditt HTML-dokument. Sökvägen bör se ut ungefär såhär:</p>
+<p>En bild säger mer än tusen rader kod brukar det sägas, vilket lyft! Nästa utmaning vi ska ta oss an är att lägga upp ikonerna för facebook, twitter och instagram på slutet av sidan och sedan gör dem klickbara. Vi börjar med ikonerna. Till skillnad från föregående bild ska vi nu försöka länka dem direkt från en mapp på datorn, det vill säga som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#211-Relativa-sokvagar">relativ sökväg</a></i>. Varje ikon kommer behöva en egen <code>&lt;img&gt;</code> tagg! Ladda ner följande <a href="/DA120A/excercises/1/ImagesLinks/images.zip">zipfil</a> och extrahera den i samma mapp som ditt HTML-dokument. Sökvägen bör se ut ungefär såhär:</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
 &lt;img src="images/namnet-på-din-bild"&gt;
 </code></pre>
