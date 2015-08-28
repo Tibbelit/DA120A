5795177ee90ec2edcd00a6d93f23946913293a1c
Jalamaal <bravoantonius@gmail.com>
2015-08-28 20:36:30 +0200

    Added changes to Images-tutorial

--- exercises/1/ImagesLinks/index.php
+++ exercises/1/ImagesLinks/index.php
@@ -6,7 +6,7 @@
             <p><a href="/exercises/1/ImagesLinks/ex2.png" data-lightbox="Webbsida" data-title="Exempellösning"><img src="/exercises/1/ImagesLinks/ex2.png"                    style="width:250px; box-shadow:0 0 5px #000; display:block; margin:auto;">.</a></p>
     <h3>2. Huvudbild - absolut sökväg</h2>
         <p>Det första vi kan börja med är att leta upp en trevlig bild på en restaurang. Har du ingen inspiration kan du ta denna 
-            <a href="http://www.svenskamassan.se/Global/svenskamassan/bilder/restaurang.jpg">bild</a>. Använd hela länkens adress och stoppa i den i din tagg som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#212-Absoluta-sokvagar">absolut sökväg</a></i>. Vegano önskar ha bilden precis under sina huvudrubriker, ungefär såhär.</p>
+            <a href="http://www.svenskamassan.se/Global/svenskamassan/bilder/restaurang.jpg">bild</a>. Använd hela länkens adress och stoppa i den i din tagg som en <i><a href="/lectures.php?type=Modul1&nr=Images#212-Absoluta-sokvagar">absolut sökväg</a></i>. Vegano önskar ha bilden precis under sina huvudrubriker, ungefär såhär.</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
 &lt;!doctype html&gt;
 &lt;html&gt;
@@ -22,7 +22,7 @@
 </code></pre> <!--Kan man gömma detta och låta studenten trycka på en knapp för att se lösningen?-->
 
 <h3>3. Ikoner - relativ sökväg</h3>
-<p>En bild säger mer än tusen rader kod brukar det sägas, vilket lyft! Nästa utmaning vi ska ta oss an är att lägga upp ikonerna för facebook, twitter och instagram på slutet av sidan och sedan gör dem klickbara. Vi börjar med ikonerna. Till skillnad från föregående bild ska vi nu försöka länka dem direkt från en mapp på datorn, det vill säga som en <i><a href="http://localhost:1234/lectures.php?type=Modul1&nr=Images#211-Relativa-sokvagar">relativ sökväg</a></i>. Varje ikon kommer behöva en egen <code>&lt;img&gt;</code> tagg! Ladda ner följande <a href="/DA120A/excercises/1/ImagesLinks/images.zip">zipfil</a> och extrahera den i samma mapp som ditt HTML-dokument. Sökvägen bör se ut ungefär såhär:</p>
+<p>En bild säger mer än tusen rader kod brukar det sägas, vilket lyft! Nästa utmaning vi ska ta oss an är att lägga upp ikonerna för facebook, twitter och instagram på slutet av sidan och sedan gör dem klickbara. Vi börjar med ikonerna. Till skillnad från föregående bild ska vi nu försöka länka dem direkt från en mapp på datorn, det vill säga som en <i><a href="/lectures.php?type=Modul1&nr=Images#211-Relativa-sokvagar">relativ sökväg</a></i>. Varje ikon kommer behöva en egen <code>&lt;img&gt;</code> tagg! Ladda ner följande <a href="/exercises/1/ImagesLinks/images.zip">zipfil</a> och extrahera den i samma mapp som ditt HTML-dokument. Sökvägen bör se ut ungefär såhär:</p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
 &lt;img src="images/namnet-på-din-bild"&gt;
 </code></pre>
@@ -32,7 +32,7 @@
 
 
 <h3>3. Klickbara ikoner</h3>
-<p>I <a href="http://localhost:1234/lectures.php?type=Modul1&nr=Links">länkföreläsningen</a> bekantade vi oss med <code>&lt;a&gt;</code> (står för eng. <i>anchor</i>) där vi kan ange relativa eller absoluta sökvägar till en annan plats. Det roliga är att bilder/ikoner också kan vara länkar! För att göra en klickbara ikoner behöver vi definiera dels vad det är som klickas på samt var vi hamnar när vi klickar på det. Du behöver därför "nästla" (eng. <i>nesting</i>) ikonerna innanför din <code>&lt;a&gt;</code> tagg: </p>
+<p>I <a href="/lectures.php?type=Modul1&nr=Links">länkföreläsningen</a> bekantade vi oss med <code>&lt;a&gt;</code> (står för eng. <i>anchor</i>) där vi kan ange relativa eller absoluta sökvägar till en annan plats. Det roliga är att bilder/ikoner också kan vara länkar! För att göra en klickbara ikoner behöver vi definiera dels vad det är som klickas på samt var vi hamnar när vi klickar på det. Du behöver därför "nästla" (eng. <i>nesting</i>) ikonerna innanför din <code>&lt;a&gt;</code> tagg: </p>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
 &lt;a href="länk-till-annan-sida"&gt;&lt;img src="images/namnet-på-din-bild"&gt;&lt;/a&gt;
 </code></pre>
@@ -50,4 +50,3 @@
 
 <p>Klarat det? Bra jobbat! Nu har vi lagt till lite trevligare estetik på sidan, mera interaktion och kommit ett steg närmare rätt århundrade. </p>
 
-<p>Exempel på bilder hittar du <a href="/exercises/1/ImagesLinks/images.zip">här (ZIP-fil)</a>.</p>
