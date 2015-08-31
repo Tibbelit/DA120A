4790d5e672bd350f3d12f478353548e4905c84ad
Anton Tibblin <tibbelit@gmail.com>
2015-08-31 16:16:22 +0200

    Added into lecture

--- lectures/Intro/intro/index.php
+++ lectures/Intro/intro/index.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-lg-8">
 		<h2>Videoföreläsning - Kursintroduktion</h2>
-		<iframe width="640" height="360" src="//www.youtube.com/embed/5CiurDCmwP8" frameborder="0" allowfullscreen></iframe>
+		<iframe width="640" height="360" src="https://www.youtube.com/embed/XTSjE5WAqGU" frameborder="0" allowfullscreen></iframe>
 		<div style="clear:both"></div>
 	</div>
 </div>
