e7815345e35acc22d485835fd004af9aae0ff9b6
Anton Tibblin <tibbelit@gmail.com>
2015-09-06 22:47:04 +0200

    Fixed CSS for mobile layout

--- _header.php
+++ _header.php
@@ -56,7 +56,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="/">DA120A: Webbutveckling</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
